2485e3e92197012d0797cfd20932c281e2c7d974
TECH BE <96684582+W55BURAK@users.noreply.github.com>
2022-05-28 11:54:11 +0300

    Chat 3000 fake version No virus
    No virus the program can speak

---
+++ Chat 3000.cmd
@@ -0,0 +1,20 @@
+@echo off
+title Chat3000
+echo Lutfen kullanici adi
+set/p Kullanıcı AD=
+echo Peki Soyadin?
+set/p KULLANICI PASSCODE=
+echo Merhaba ad bilgileriniz doğru mu ?%Kullanıcı AD% %KULLANICI PASSCODE%
+set/p cevap=
+echo Hosgeldiniz chat 3000 hazir
+set/p cevap=
+echo Mesajiniz gönderilemiye bilir
+set/p cevap=
+echo mesajiniz?
+set/p cevap=
+echo Sistem yeniden başlatilacak 
+echo Sistem açıldı
+echo OYUN GAMES YENI SISTEM
+echo New system avvible!
+echo Download for ATP system updater v1.2
+pause
